2e46103b13edd9c9b6f3ca4be13b27ca708917f8
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-24 14:31:17 -0400

    import profile to passport file

--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,7 @@ import passport from 'passport'
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
 
 import { User } from '../models/user.js'
+import { Profile } from '../models/profile.js'
 
 passport.use(
   new GoogleStrategy(
@@ -12,36 +13,5 @@ passport.use(
       callbackURL: process.env.GOOGLE_CALLBACK,
     },
     function (accessToken, refreshToken, profile, done) {
-      User.findOne({ googleId: profile.id }, function (err, user) {
-        if (err) return done(err)
-        if (user) {
-          return done(null, user)
-        } else {
-          // we have a new student via OAuth!
-          const newStudent = new Student({
-            name: profile.displayName,
-            avatar: profile.photos[0].value,
-          })
-          // Build the user from 
-          const newUser = new User({
-            email: profile.emails[0].value,
-            googleId: profile.id,
-            studentProfile: newStudent._id
-          })
-          newStudent.save(function (err) {
-            if (err) return done(err)
-          })
-          newUser.save(function (err) {
-            if (err) {
-              // Something went wrong while making a user - delete the profile
-              // we just created to prevent orphan profiles.
-              Student.findByIdAndDelete(newStudent._id)
-              return done(err)
-            }
-            return done(null, newUser)
-          })
-        }
-      })
     }
-  )
-)
+  
