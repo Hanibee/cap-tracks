138777c8e0a6a88dc76196d048f393e2bcc21dea
Caleb Vietinghoff <vietinghoffcaleb@gmail.com>
2022-03-24 14:19:16 -0400

    call oauth strategy into config file

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,15 @@
 import passport from 'passport'
+
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK,
+    },
+    function (accessToken, refreshToken, profile, done) {
+    }
+  )
+)
